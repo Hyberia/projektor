8de4a017a99dc4cd72ac197c52615557f94a95e6
Mathieu Charron (Luna) <mathieu.charron@elwillow.net>
2009-06-25 16:46:02 -0400

    added config file and original script

--- run.py
+++ run.py
@@ -1,1 +1,49 @@
+#!/usr/bin/python
+"""Main script for the Touei project. Need the Touei modules"""
 
+__author__ = "Mathieu Charron"
+__license__ = "Eiffel Version 2"
+__version__ = "0.1"
+__revision__ = ""
+
+# Configuration
+
+# Set this to your configuration file.
+CONFIG_FILE = "touei.conf"
+
+
+###
+# No not change anything below this!
+###
+
+# Importation!
+import logging
+import logging.config
+import ConfigParser
+import time
+import os
+import sys
+import touei
+
+
+if __name__ == "__main__":
+    # Create the configParser instance
+    config = ConfigParser.SafeConfigParser(touei.CONFIG_DEFAULT_VALUE)
+    print config
+    # Check if the config file exists:
+    if os.path.exists(CONFIG_FILE):
+        print "Loading config"
+        config.read(CONFIG_FILE)
+    else: print "Cannot load config"
+
+    # Create Logger
+    logging.basicConfig(filename=config.get('logs','main_log'), level=config.getint('logs', 'level'))
+    touei_logger = logging.getLogger("touei")
+
+    # Create the console logging
+    console = logging.StreamHandler()
+    logging.getLogger('touei_logger').addHandler(console)
+    print config.get('logs','main_log'), config.get('logs', 'level')
+    print logging.WARNING
+    #Check if it works
+    touei_logger.critical("Application run complete")
--- touei/__init__.py
+++ touei/__init__.py
@@ -5,6 +5,22 @@ __version__ = "0.1"
 
 __all__ = ["player", "playlist", "mkvutils", "utils"]
 
+CONFIG_DEFAULT_VALUE={"pid": "/var/pid/touei.pid",
+                      "slave_socket": "/tmp/touei_slave.fifo",
+                      "deamon_log": "touei_deamon.log",
+                      "main_log": "touei_main.log",
+                      "level": "30",
+                      "seek_delay": "2",
+                      "recovery_time": "70",
+                      "block_duration": "60",
+                      "location": "/home/video",
+                      "recovery": "%(location)/recovery.mkv",
+                      "standby": "%(location)/standby.mkv",
+                      "intro": "%(location)/intro.mkv",
+                      "outro": "%(location)/outro.mkv",
+                      "countdown": "%(location)/countdown.mkv",
+                      }
+
 
 # Load sub modules
 from mkvutils import *
