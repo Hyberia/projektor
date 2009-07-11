7e27b9079fa653a3476029f48236cbd6ae147aa4
Mathieu Charron (shinku) <mathieu.charron@elwillow.net>
2009-07-11 15:39:34 -0400

    * Updated license text to Effiel Forum License 2.0 * Fix some url links

--- touei/mkvutils.py
+++ touei/mkvutils.py
@@ -1,11 +1,29 @@
 # Writer: Mathieu
 # Date: 10 Juin 2009
+#
+# Eiffel Forum License, version 2
+#
+# 1. Permission is hereby granted to use, copy, modify and/or
+#    distribute this package, provided that:
+#       * copyright notices are retained unchanged,
+#       * any distribution of this package, whether modified or not,
+#         includes this license text.
+# 2. Permission is hereby also granted to distribute binary programs
+#    which depend on this package. If the binary program depends on a
+#    modified version of this package, you are encouraged to publicly
+#    release the modified version of this package.
+#
+# THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT WARRANTY. ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHORS BE LIABLE TO ANY PARTY FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THIS PACKAGE.
 
-# COPYRIGHT NOTICE
 
 """
 this is the mkvutils module part of the Touei project.
-please see http://elwillow.net/x/touei for more info.
+please see http://elwillow.net/touei for more info.
 """
 
 __version__ = "0.1"
