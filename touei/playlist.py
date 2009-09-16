5e7e21aed86fea70bee32a777c0b9830b148302f
Martin Samson <masom@xephon>
2009-09-15 23:15:53 -0400

    minimal working stuff: sqlite3 memory database files listing

--- touei/playlist.py
+++ touei/playlist.py
@@ -1,4 +1,4 @@
-# Writer: Mathieu
+# Writer: Mathieu,Martin
 # Date: 10 Juin 2009
 #
 # Eiffel Forum License, version 2
@@ -27,5 +27,97 @@ please see http://elwillow.net/touei for more info.
 
 __version__ = "0.1"
 
-class PlaylistMgmt():
-    pass
+
+import sqlite3 as sqlite
+import os,sys,datetime
+
+class DBUnavailableException(Exception): pass
+class DBExistsException(Exception): pass
+
+class PlayList():
+    def __init__(self):
+        self._db = None
+        self._schema = []
+        self._schema.append('''create table presentations(
+                            id integer primary key autoincrement,
+                            date int,
+                            file varchar(255),
+                            title varchar(255),
+                            duration int
+                            )''')
+        
+        self._schema.append('''create table fillers(
+                            id integer primary key autoincrement,
+                            file varchar(255),
+                            title varchar(255),
+                            duration int
+                            )''')
+        
+        self._date = datetime.datetime.now()
+        
+    def _getFormattedTime(self):
+        return self._date.strftime("%H%M")
+
+    def _CreateDb(self):
+        '''Create the database if it is not already initialized.'''
+        
+        if self._db:
+            raise DBExistsException()
+        
+        #Create the database in RAM instead of on-disk.
+        try:
+            self._db = sqlite.connect(":memory:")
+        except Exception,e:
+            raise e
+        
+        cursor = None
+        try:
+            cursor = self._db.cursor()
+            for table in self._schema:
+                cursor.execute(table)
+            self._db.commit()
+        except Exception,e:
+            self._db.rollback()
+            raise e
+        finally:
+            if cursor: cursor.close()
+            
+        return True
+    
+    def _CloseDb(self):
+        '''Close the database connection'''
+        if not self._db:
+            raise DBUnavailableException()
+        try:
+            self._db.close()
+        except Exception,e:
+            return False
+        
+        self._db = None
+        return True
+    
+    def _parseFiles(self):
+        '''Parses files and prepare them for playback.'''
+        presentations = self._getFiles('/home/masom/video')
+        
+        if len(presentations.keys()) == 0:
+            raise NoFilesException()
+        
+        print presentations
+        
+    
+    def _getFiles(self,dir):
+        '''Get all the mkv files from dir (and bellow)'''
+        videos = {}
+        for path, dirs, files in os.walk(dir):
+        
+            for file in files:
+                if not file.endswith('.mkv'):
+                    continue
+                
+                if not path in videos.keys():
+                    videos[path] = []
+                    
+                videos[path].append(file)
+                
+        return videos
