59028389f73809e7e6cfe7bfd783a7a19eda4696
Mathieu Charron <mathieu.charron@elwillow.net>
2009-11-08 22:17:28 -0500

    ASS + mkv muxing complete. Missing loggin stuff.

--- touei/mkvutils.py
+++ touei/mkvutils.py
@@ -33,7 +33,8 @@ __license__ = "Eiffel Version 2"
 __version__ = "0.1"
 __revision__ = ""
 
-
+# Editing the following will affect all ASS file generated within this
+# script.
 ASS_HEADER = """[Script Info]
 ; This script was created by subtitleeditor (0.30.0)
 ; http://home.gna.org/subtitleeditor/
@@ -47,7 +48,7 @@ Collisions: Normal
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
 Style: default,DejaVu Sans,18,&H000000E8,&H00FFFFFF,&H00000000,&H7ADDDDE2,-1,0,0,0,100,100,0,0,1,1,1,2,20,20,20,1
-Style: Top,DejaVu Sans,18,&H000000E8,&H00FFFFFF,&H00000000,&H7FDDDDE2,-1,0,0,0,100,100,0,0,1,1,1,7,20,20,20,0
+Style: top,DejaVu Sans,18,&H000000E8,&H00FFFFFF,&H00000000,&H7FDDDDE2,-1,0,0,0,100,100,0,0,1,1,1,7,20,20,20,0
 [Events]
 Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text
 """
@@ -57,7 +58,7 @@ Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text
 # Second: End time (0:00:00.00)
 # third: The style (check in the header)
 # Last: the text itself
-ASS_EVENT = "Dialogue: 0,%s,%s,%s,,0000,0000,0000,,{\\fad(250,250)}%s"
+ASS_EVENT = "Dialogue: 0,%s,%s,%s,,0000,0000,0000,,{\\fad(250,250)}%s\n"
 import subprocess,re
 class MkvUtils():
 
@@ -72,8 +73,8 @@ class MkvUtils():
 
         # Run the command
         try:
-            mkvinfo = subprocess.Popen(['mkvinfo', fileName], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, \
-                                                    close_fds=True)
+            mkvinfo = subprocess.Popen(['mkvinfo', fileName], shell=False, \
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
         except Exception,e:
             print e
             return 0
@@ -93,7 +94,7 @@ class MkvUtils():
             return 0
 
         return seconds
-    def _gen_event(self, time_start, duration, style, text):
+    def _gen_event(self, timeStart, duration, style, text):
         """Generate a event string for the ASS generation
         @param string time_start When to display, in seconds
         @param string duration for how long, in seconds
@@ -102,8 +103,8 @@ class MkvUtils():
         @return string the generated string.
         """
         # Time work
-        sstime = "0:%02i:%02i.00" % (time_start/60, time_start%60)
-        etime = time_start + duration
+        sstime = "0:%02i:%02i.00" % (timeStart/60, timeStart%60)
+        etime = timeStart + duration
         setime = "0:%02i:%02i.00" % (etime/60, etime%60)
         return ASS_EVENT % (sstime, setime, style, text)
 
@@ -121,7 +122,6 @@ class MkvUtils():
         """
         # Get the filename
         ass_filename = presentation[:len(presentation)-3]+"ass"
-        print ass_filename
         return os.path.exists("%s/ass/%s"% (debug_config_tmp, ass_filename))
 
     def _gen_ass(self, presentation):
@@ -130,17 +130,18 @@ class MkvUtils():
         @return boolean True if error, False if okay
         """
         # Get the filename
-        ass_filename = presentation[:len(presentation)-3]+"ass"
-        presentation_text = presentation.split(".")[1].replace("_", " ").title()
+        assFilename = presentation[:len(presentation)-3]+"ass"
+        dispText = presentation.split(".")[1].replace("_", " ").title()
         # Open the file
-        ass_file = open("%s/ass/%s"% (debug_config_tmp, ass_filename), 'w')
+        assFile = open("%s/ass/%s" % (debug_config_tmp, assFilename), 'w')
         # Write the header
-        ass_file.write(ASS_HEADER)
-        # Create event
-        ass_file.write(self._gen_event(10,2,'default',presentation_text))
+        assFile.write(ASS_HEADER)
+        # Create events
+        assFile.write(self._gen_event(0,6,'top',"Sponsored by Magmic Games"))
+        assFile.write(self._gen_event(1,3,'default',"Next Presentation: " + dispText))
+        assFile.write(self._gen_event(4,3,'default',"Prochaine Presentation: " + dispText))
         # Close the file
-        ass_file.write("\n")
-        ass_file.close()
+        assFile.close()
         return 0
 
     def _mux_mkv(self, presentation):
@@ -148,8 +149,14 @@ class MkvUtils():
         @param string the presentation
         @return boolean True if error, False if okay
         """
-        # Get the filename
-        ass_filename = presentation.split(".")[1][:len(presentation)-3]+"ass"
+        # Get the filenames
+        assFilename = presentation[:len(presentation)-3]+"ass"
+        assLocation = "%s/ass/%s" % (debug_config_tmp, assFilename)
+        intro_output = "%s/intro/%s" % (debug_config_tmp, presentation)
+        # Generate the MKV
+        muxingCmd = subprocess.call(["mkvmerge", "-o", intro_output, assLocation, debug_config_intro], \
+                    shell=False, close_fds=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,)
+        return muxingCmd
 
     def generate_intro(self, presentation):
         """Primary function. Il will all the sanity check before
@@ -160,29 +167,31 @@ class MkvUtils():
         """
         # check if the file exist
         if self._already_assed(presentation):
-            print "ASS already exist"
+            print "ASS: Present"
             # Do we have a MKV?
             if self._already_muxed(presentation):
                 # We do... do nothing
-                print "doing nothing."
+                print "MKV: Present"
                 return 0
             else:
                 # We'll take the already create ASS for the muxing
-                print "create MKV from already existing ASS."
+                print "MKV: Generating..."
                 self._mux_mkv(presentation)
                 return 0
         else:
-        # Do we have a intro MKV?
+            print "ASS: Missing"
+            # Do we have a intro MKV?
             if self._already_muxed(presentation):
                 # strange, no ass but there is a MKV for the intro.
                 # Won't do a thing
-                print "Doing nothing."
+                print "MKV: Present"
                 return 0
             else:
+                print "MKV: Missing"
                 # We have to generate the whole intro file
-                print "Generating ASS."
+                print "ASS: Generating..."
                 self._gen_ass(presentation)
-                print "Generating MKV."
+                print "ASS: Generating..."
                 self._mux_mkv(presentation)
                 return 0
 
