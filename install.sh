8488b8e2a3bb8c2ded318ec461df59931ed34e4e
Jamie Nadeau <james2432@gmail.com>
2009-12-17 14:43:18 -0500

    Modified install script to run in bash Accepts install,uninstall,clean as arguments TODO: get python to uninstall it's crap

--- install.sh
+++ install.sh
@@ -1,21 +1,81 @@
-#!/bin/sh
+#!/bin/bash
 # Author:      G-Anime
-# Contributor: Mathieu Charron
+# Contributor: Mathieu Charron, Jamie Nadeau
 # Version:     0.1
 # Revision     39
 #
 # Copyright 2008, Author Name, example.org
 # Licensed under the Eiffel Forum License 2.
 
+# make sure we're running as root
+if [ "$UID" != "0" ]; then
+	echo "Sorry, must be root. Exiting..."
+  	exit
+fi
 
-echo "Building libReadToueiConfig"
-cd readconfig
-make $1
-cd ..
 
-echo "Building toueid"
-cd toueid
-make $1
-cd ..
+#Get number of arguments
+COMMAND=
 
-python setup.py $1
+
+if ["$1"=""]; then
+   COMMAND='install'
+else
+   COMMAND=$1
+fi
+
+case "$COMMAND" in
+'install')
+	echo "Building libReadToueiConfig"
+	cd readconfig
+	make
+
+	echo "Installing libReadToueiConfig"
+	make $COMMAND
+	cd ..
+
+	echo "Building toueid"
+	cd toueid
+	make
+	
+	echo "Installing toueid"
+	make $COMMAND
+	cd ..
+
+	echo "Installing touei"
+	python setup.py --record $COMMAND
+	
+#end of install
+;;
+'uninstall')
+	echo "Uninstalling libReadToueiConfig"
+	cd readconfig
+	make $COMMAND
+	cd ..
+
+	echo "Uninstalling toueid"	
+	cd toueid
+	make $COMMAND
+	cd ..
+	
+	echo "Uninstalling touei"
+	python setup.py $COMMAND
+#end uninstall
+;;
+'clean')
+	echo "Cleaning libReadToueiConfig"
+	cd readconfig
+	make $COMMAND
+	cd ..
+
+	echo "Cleaning toueid"	
+	cd toueid
+	make $COMMAND
+	cd ..
+	
+	echo "Cleaning touei"
+	python setup.py $COMMAND
+;;
+esac
+
+
